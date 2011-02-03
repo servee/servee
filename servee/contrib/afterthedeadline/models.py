dfadc9cef26f506216e3b8e77f554c0beb1072e4
Issac Kelly <issac.kelly@gmail.com>
2011-02-03 10:55:41 -0500

    Remove ATD as it doesn't work

--- servee/contrib/afterthedeadline/models.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('servee.contrib.afterthedeadline.views',
-    url(r'^afterthedeadline/$', view='afterthedeadline', name='afterthedeadline'),
-)
