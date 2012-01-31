214e14517e0d73a576ef4d9d8c6f57cfcc068c1d
Daniel Lindsley <daniel@toastdriven.com>
2012-01-31 09:45:16 -0500

    Clean-roomed the ``space_out_camel_case`` function.

--- servee/utils.py
+++ servee/utils.py
@@ -1,20 +1,16 @@
-import re
-
-def space_out_camel_case(stringAsCamelCase):
+def space_out_camel_case(camel):
     """
-    @@TODO white-box implementation of this function from somebody who hasn't seen the code.
-    
-    Adds spaces to a camel case string.  Failure to space out string returns the original string.
-    >>> space_out_camel_case('DMLSServicesOtherBSTextLLC')
-    'DMLS Services Other BS Text LLC'
-    
-    Originally by Simon Hartley 12/27/2008, tbh, I'm not sure of the licensing presumptions of
-    a site like the below...which means I probably don't have the right to use this.
-    http://refactormycode.com/codes/675-camelcase-to-camel-case-python-newbie#refactor_139268
+    Converts a "CamelCasedName" to "Camel Case Name".
     """
-    
-    if stringAsCamelCase is None:
-        return None
+    chars = []
 
-    pattern = re.compile('([A-Z][A-Z][a-z])|([a-z][A-Z])')
-    return pattern.sub(lambda m: m.group()[:1] + " " + m.group()[1:], stringAsCamelCase)
+    for char in camel:
+        if len(chars) >= 2 and chars[-1] != ' ':
+            if char.isupper() and chars[-1].islower():
+                chars.append(' ')
+            elif char.islower() and chars[-1].isupper() and chars[-2].isupper():
+                chars.insert(len(chars) - 1, ' ')
+
+        chars.append(char)
+
+    return ''.join(chars)
