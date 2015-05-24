ff8c14b949630fd778e3d9f500f245a15db48ec5
Issac Kelly <issac.kelly@gmail.com>
2015-05-24 13:48:28 -0700

    Update templates for 1.8

--- servee/frontendadmin/templatetags/servee_admin_modify.py
+++ servee/frontendadmin/templatetags/servee_admin_modify.py
@@ -2,6 +2,8 @@ from django import template
 
 register = template.Library()
 
+
+@register.inclusion_tag('servee/_submit_line.html', takes_context=True)
 def submit_row_frontendadmin(context):
     """
     Displays the row of buttons for delete and save.
@@ -10,16 +12,24 @@ def submit_row_frontendadmin(context):
     change = context['change']
     is_popup = context['is_popup']
     save_as = context['save_as']
-    return {
-        'onclick_attrib': (opts.get_ordered_objects() and change
-                            and 'onclick="submitOrderForm();"' or ''),
-        'show_delete_link': (not is_popup and context['has_delete_permission']
-                              and (change or context['show_delete'])),
+    show_save = context.get('show_save', True)
+    show_save_and_continue = context.get('show_save_and_continue', True)
+    ctx = {
+        'opts': opts,
+        'show_delete_link': (
+            not is_popup and context['has_delete_permission'] and
+            change and context.get('show_delete', True)
+        ),
         'show_save_as_new': not is_popup and change and save_as,
-        'show_save_and_add_another': context['has_add_permission'] and
-                            not is_popup and (not save_as or context['add']),
-        'show_save_and_continue': not is_popup and context['has_change_permission'],
+        'show_save_and_add_another': (
+            context['has_add_permission'] and not is_popup and
+            (not save_as or context['add'])
+        ),
+        'show_save_and_continue': not is_popup and context['has_change_permission'] and show_save_and_continue,
         'is_popup': is_popup,
-        'show_save': True
+        'show_save': show_save,
+        'preserved_filters': context.get('preserved_filters'),
     }
-register.inclusion_tag('servee/_submit_line.html', takes_context=True)(submit_row_frontendadmin)
+    if context.get('original') is not None:
+        ctx['original'] = context['original']
+    return ctx
