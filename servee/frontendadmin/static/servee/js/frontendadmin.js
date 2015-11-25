c850ec5a66488312b9a54276d2b2898f0ad0376d
Kasey Kelly <kasey@servee.com>
2015-11-25 17:12:07 -0500

    adding gallery list edits back in

--- servee/frontendadmin/static/servee/js/frontendadmin.js
+++ servee/frontendadmin/static/servee/js/frontendadmin.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 
     function set_parent($link){
@@ -76,6 +75,58 @@ $(document).ready(function(){
             url: this.href,
             success: function(data, text){
                 $par.html(data);
+                $('.file-upload a').not("[href*='.pdf'], [href*='.doc'], [href*='.mp3']").each(function(){
+                    var thumbURL = $(this).attr('href');
+                    if (thumbURL){
+                        $(this).prepend('<img src="' + thumbURL + '" style="max-width:180px;max-height:100px;margin-bottom:10px;display:block;">');
+                    }
+                });
+                // hide galleries, because they make the form crazy long
+                $('#images-group').before("<a href='#' id='gallery_dropdown'>Edit the image gallery for this page <span>›</span></a>");
+                $('#images-group').hide();
+                $('#items-group').before("<a href='#' id='gallery_dropdown'>Edit the image gallery for this project <span>›</span></a>");
+                $('#items-group').hide();
+
+                function labelTables(){
+                    $('#servee_container .module table').each(function(){
+                        // I changed the django "module" table into a vertical form display using some
+                        // CSS trickery. Here, I take the labels from the table display and insert
+                        // them into the right places with CSS. Since pseudo-elements can't be applied
+                        // inline, this is sort of complicated. The CSS is taking the text from the
+                        // data-content attribute, which I'm adding below. --KK
+                        var head1 = $(this).find("th:nth-of-type(1)").text();
+                        var head2 = $(this).find("th:nth-of-type(2)").text();
+                        var head3 = $(this).find("th:nth-of-type(3)").text();
+                        var head4 = $(this).find("th:nth-of-type(4)").text();
+                        var head5 = $(this).find("th:nth-of-type(5)").text();
+                        var head6 = $(this).find("th:nth-of-type(6)").text();
+                        var head7 = $(this).find("th:nth-of-type(7)").text();
+                        var head8 = $(this).find("th:nth-of-type(8)").text();
+                        var head9 = $(this).find("th:nth-of-type(9)").text();
+                        var head10 = $(this).find("th:nth-of-type(10)").text();
+
+                        $("td:nth-of-type(2)").attr('data-content', head1 );
+                        $("td:nth-of-type(3)").attr('data-content', head2 );
+                        $("td:nth-of-type(4)").attr('data-content', head3 );
+                        $("td:nth-of-type(5)").attr('data-content', head4 );
+                        $("td:nth-of-type(6)").attr('data-content', head5 );
+                        $("td:nth-of-type(7)").attr('data-content', head6 );
+                        $("td:nth-of-type(8)").attr('data-content', head7 );
+                        $("td:nth-of-type(9)").attr('data-content', head8 );
+                        $("td:nth-of-type(10)").attr('data-content', head9 );
+                        $("td:nth-of-type(11)").attr('data-content', head10 );
+                        $("thead").hide();
+                    });
+
+                };
+                $('#gallery_dropdown').on("click", function(e){
+                   $('#images-group').toggle();
+                   $('#items-group').toggle();
+                   labelTables();
+                   e.preventDefault();
+                   return false;
+                });
+                labelTables();
             }
         });
         e.preventDefault();
